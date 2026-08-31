b9c91ad7bc0c8d19bc1ad821cfbc8843bbd54d62
Celeste <luciacelestezarate01@gmail.com>
2026-08-31 19:26:37 -0300

    feat: crear endpoint de usuarios en formato JSON

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,18 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ListarPersonasController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route:: get('/', function () {
+    return view('welcome');
+});
+#ruta que apunta al métdo index del controlador
+Route:: get ('/usuarios', [UserController::class, 'index']);
 
 Route:: get('/personas', [ListarPersonasController::class,'index']);
 
